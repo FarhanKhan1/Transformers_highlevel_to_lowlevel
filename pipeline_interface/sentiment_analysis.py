400cf273a2da6ef50af0aa602b795b93927386b0
Farhankhan1 <enggfarhan36@gmail.com>
2026-07-18 17:25:04 +0500

    Initial commit

---
+++ pipeline_interface/sentiment_analysis.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+print("Loading model...")
+classifier = pipeline("sentiment-analysis")
+print("Ready!")
+
+while True:
+    text = input("Enter text (q to quit): ")
+
+    if text.lower() == "q":
+        break
+
+    print(classifier(text))
